c0012054e00c849a3d851826ecc015813b802b8c
Darick <darickquinto@yahoo.com>
2020-10-05 19:06:19 +0800

    i revised the  from the http request in order to match the add_row functionality of ACF as it does not work after i change it from update_field to add_row function because the data structure is incorrect

--- public/endpoints/perthwake-park-app-endpoint-update-user-by-id-class.php
+++ public/endpoints/perthwake-park-app-endpoint-update-user-by-id-class.php
@@ -20,7 +20,7 @@ class UPDATE_Users_by_ID_Route {
     // Route for Editing the users waivers
     public function pwp_put_user_by_ID( WP_REST_Request $request ) {
  
-        $data = $request['waivers'];
+        //$data = $request['waivers'];
         $user_fname = $request['first_name'];
         $user_lname = $request['last_name'];
         //$birthdate = array(array( "dob" => $request['birthdate'] ));
@@ -36,6 +36,18 @@ class UPDATE_Users_by_ID_Route {
 
         //update_field( $field_key, $data, "user_{$request['id']}" ); 
         //update_field( $field_key, $birthdate, "user_{$request['id']}" ); 
+        
+		$data = array(
+		    'completed' => $request['waivers'][0]['completed'],
+			'name' => $request['waivers'][0]['name'],
+            "phone" => $request['waivers'][0]['phone'],
+            "dob" => $request['waivers'][0]['dob'],
+            "postcode" => $request['waivers'][0]['postcode'],
+            "country" => $request['waivers'][0]['country'],
+            "agreement" => $request['waivers'][0]['agreement'],	
+            "emergency_contact" => $request['waivers'][0]['emergency_contact'],	
+            "additional_minors" => $request['waivers'][0]['additional_minors'],	
+		);
         
         add_row( $field_key, $data, "user_{$request['id']}" ); 
 
